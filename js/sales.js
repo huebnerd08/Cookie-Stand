9a5e652498c9c77bdac240ee9b6f39883fe92320
Dashiell <huebnerd08@gmail.com>
2018-09-17 15:39:16 -0700

    defined object properties for each shop location

--- js/sales.js
+++ js/sales.js
@@ -1,1 +1,43 @@
 'use strict';
+
+//Stores the min/max hourly customers, and the average cookies per customer, in object properties
+
+//Create object tree with parent object = shop
+
+var shop = {
+  location: [    //define properties for each location
+    {
+      address: '1st and Pike', //key = address; value = 1st and pike
+      minCost: 23,
+      maxCost: 65,
+      avgCookieSale: 6.3
+    },
+    {
+      address: 'SeaTac Airport',
+      minCost: 3,
+      maxCost: 24,
+      avgCookieSale: 1.2
+    },
+    {
+      address: 'Seattle Center',
+      minCost: 11,
+      maxCost: 38,
+      avgCookieSale: 3.7
+    },
+    {
+      address: 'Capitol Hill',
+      minCost: 20,
+      maxCost: 38,
+      avgCookieSale: 2.3
+    },
+    {
+      address: 'Alki',
+      minCost: 2,
+      maxCost: 16,
+      avgCookieSale: 4.6
+    },
+  ],
+
+  var address = ''
+
+}
